d89d0323d1d5d7a9ef6d64f0256f0abbffbf4002
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-02-12 10:18:03 +0530

    add cart store using redux tool kit

---
+++ src/store/cart-slice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+  totalPrice: 0,
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: { initialState },
+  reducers: {
+    addItemTocart(state, action) {
+      const item = action.payload;
+      const existingItem = state.items.find((data) => data.id === item.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: item.id,
+          price: item.price,
+          quantity: 1,
+          totalPrice: item.price,
+          name: item.title,
+        });
+      } else {
+        existingItem.quantity = existingItem.quantity + 1;
+        existingItem.totalPrice = existingItem.totalPrice + item.totalPrice;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
