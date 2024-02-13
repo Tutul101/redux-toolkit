84a25ff5240952ab0472164645fc9eed265297ac
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-02-14 00:23:10 +0530

    add to cart remove cart feature

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -3,18 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   items: [],
   totalQuantity: 0,
-  totalPrice: 0,
 };
 const cartSlice = createSlice({
   name: "cart",
-  initialState: { initialState },
+  initialState: initialState,
   reducers: {
     addItemTocart(state, action) {
       const item = action.payload;
       const existingItem = state.items.find((data) => data.id === item.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: item.id,
+          id: item.id,
           price: item.price,
           quantity: 1,
           totalPrice: item.price,
@@ -22,12 +22,13 @@ const cartSlice = createSlice({
         });
       } else {
         existingItem.quantity = existingItem.quantity + 1;
-        existingItem.totalPrice = existingItem.totalPrice + item.totalPrice;
+        existingItem.totalPrice = existingItem.totalPrice + item.price;
       }
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
